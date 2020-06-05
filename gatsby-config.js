c6f4cef3a33740e006b433d5882336efe37d1b04
Angel Lacret <alacret@gmail.com>
2020-06-05 18:41:23 -0400

    testing deployment with AWS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,148 +1,34 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 module.exports = {
   siteMetadata: {
-    title: 'Cobuild Lab Development Blog',
-    author: 'cobuildlab',
-    about: `Breakfast procuring no end happiness allowance assurance frank. Met simplicity nor difficulty unreserved who. Entreaties mr conviction dissimilar me
-    astonished estimating cultivated.`, // TODO update this
-    description: 'Software development in Miami, Florida',
-    siteUrl: 'https://storyhub-personal-tarex.redq.now.sh', // TODO update this
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-styled-components',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        minify: false, // Breaks styles if not set to false
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: 'assets',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 590,
-              linkImagesToOriginal: true,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-katex',
-            options: {
-              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: 'ignore',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-mermaid',
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-          },
-
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-          },
-          {
-            resolve: 'gatsby-remark-smartypants',
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-sharp',
-    },
-    {
-      resolve: 'gatsby-plugin-sharp',
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-feed',
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'StoryHub - Personal Blog',
-        short_name: 'StoryHub',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'content/assets/favicon.png',
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-offline',
-    },
-    {
-      resolve: 'gatsby-plugin-react-helmet',
-    },
-    {
-      resolve: 'gatsby-plugin-typescript',
-    },
-    {
-      resolve: 'gatsby-plugin-lodash',
-    },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-        endpoint: process.env.MAIL_CHIMP_END_POINT,
-      },
-    },
-    {
-      resolve: 'gatsby-source-instagram',
-      // add your instagram username, access_token and id below
-      options: {
-        // username: ,
-        // access_token: ,
-        // instagram_id: ,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-prefetch-google-fonts',
-      options: {
-        fonts: [
-          {
-            family: 'Poppins',
-            variants: ['300', '400', '500', '600', '700'],
-          },
-          {
-            family: 'Fira Sans',
-            variants: ['100', '300', '400', '500', '600', '700'],
-          },
-        ],
-      },
-    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
